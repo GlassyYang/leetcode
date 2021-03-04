e70b473042e2e37fe15dd86aed1de011707d4396
unknown <zhangya2009@live.com>
2021-03-04 19:45:57 +0800

    review 338

--- src/Solution338.java
+++ src/Solution338.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Solution338 {
 
     // 动态规划，找规律
-    public int[] countBits(int num) {
+    public int[] countBitsAns(int num) {
         int[] bits = new int[num + 1];
         int highBit = 0;
         for (int i = 1; i <= num; i++) {
@@ -12,6 +12,17 @@ public class Solution338 {
             bits[i] = bits[i - highBit] + 1;
         }
         return bits;
+    }
+
+    public int[] countBits(int num) {
+        if (num == 0) return new int[]{0};
+        int[] ans = new int[num + 1];
+        ans[0] = 0;
+        ans[1] = 1;
+        for (int i = 2; i <= num; i++) {
+            ans[i] = ans[i & (i - 1)] + 1;
+        }
+        return ans;
     }
 
 }
